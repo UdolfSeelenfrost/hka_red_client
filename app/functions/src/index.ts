80879b19f8356023200641ac406a529bccfdcbde
Matthes Würbs <m.wuerbs@gmail.com>
2023-12-02 14:29:01 +0100

    setup

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -13,7 +13,16 @@ import * as logger from "firebase-functions/logger";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+// import {onRequest} from "firebase-functions/v2/https";
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
 // });

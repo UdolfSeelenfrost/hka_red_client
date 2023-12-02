60ecb510b6683f17fe38ab8b45ad61c06ff8dd57
Matthes Würbs <m.wuerbs@gmail.com>
2023-12-02 14:37:59 +0100

    setup express

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,28 +1,31 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// /**
+//  * Import function triggers from their respective submodules:
+//  *
+//  * import {onCall} from "firebase-functions/v2/https";
+//  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+//  *
+//  * See a full list of supported triggers at https://firebase.google.com/docs/functions
+//  */
 
 // import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
 
-// const express = require('express');
-// const app = express();
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
 // });
+
+import {onRequest} from "firebase-functions/v2/https";
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, TypeScript Express!');
+});
+
+
+exports.r = onRequest(app);

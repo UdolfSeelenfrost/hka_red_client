c434dd14d1f8cab90df34888f852d26294ee7533
Matthes Würbs <m.wuerbs@gmail.com>
2024-01-06 16:33:05 +0100

    add endpoint to fill database with content (use endpoint from emulator)

--- app/functions/src/mockData.ts
+++ app/functions/src/mockData.ts
@@ -1,16 +1,30 @@
 import express, {Request, Response} from "express";
 import {getFirestore} from "firebase-admin/firestore";
+import {RedditLoader} from "./RedditLoader";
 
 export const app = express();
+const loader = new RedditLoader();
 
-app.post("/fill/:sub", (req: Request, res: Response) => {
-  res.send("not implemented");
+app.post("/fill/:sub", async (req: Request, res: Response) => {
+  const sub = req.params.sub;
+  const subData = req.body;
+  const subCollection = await getFirestore().collection(sub);
+
+  for (const post of subData.children) {
+    await subCollection.doc(post.data.name).set(post.data);
+  }
+
+  return res.send("ok");
 });
 
 app.post("/fill/:sub/:id", async (req: Request, res: Response) => {
   const sub = req.params.sub;
-  const id = req.params.id;
   const postData = req.body;
+  let id = req.params.id;
+
+  if (!id.startsWith("t3_")) {
+    id = "t3_" + id;
+  }
 
   const subCollection = await getFirestore().collection(sub);
   const doc = await subCollection.doc(id).get();
@@ -29,4 +43,39 @@ app.post("/fill/:sub/:id", async (req: Request, res: Response) => {
     await doc.ref.collection("comments").doc(comment.name).set(comment);
   }
   return res.send("ok");
+});
+
+app.get("/load/:sub", async (req: Request, res: Response) => {
+  const sub = req.params.sub;
+
+  const data = await loader.loadSub(sub, 25);
+  let result = await fetch("https://mock-3l7bazumfq-ey.a.run.app/fill/" + sub, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (result.ok) {
+    console.log("loaded and stored");
+  }
+
+  for (const post of data.children) {
+    const comments = await loader.loadComments(sub, post.data.name.substr(3), 25, 3);
+
+    let result = await fetch(`https://mock-3l7bazumfq-ey.a.run.app/fill/${sub}/${post.data.name}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(comments),
+    });
+
+    if (result.ok) {
+      console.log("loaded and stored comment");
+    }
+  }
+
+
+  return res.json(data);
 });
